4bd0ce9a9d7ba65a3c8d41ff88f6e55522c5706a
VASS\franciscojose.bermud <franciscojose.bermudez@vass.es>
2018-12-11 22:08:35 +0100

    Control de cierre de app mediante onbackpressed

--- app/src/main/java/fjbermudez/com/sportslist/app/base/BaseActivity.java
+++ app/src/main/java/fjbermudez/com/sportslist/app/base/BaseActivity.java
@@ -3,6 +3,7 @@ package fjbermudez.com.sportslist.app.base;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
@@ -15,6 +16,11 @@ public class BaseActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (fragmentManager.getBackStackEntryCount() == 1){ // close app when stay in last fragment
+            System.exit(0);
+        }
         super.onBackPressed();
     }
 }
